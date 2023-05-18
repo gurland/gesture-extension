f551332e7f251b0e496e76a9cea2d7b07646b087
mealcoSoftwareEngineer1 <ilya@mealco.co>
2023-05-18 17:59:51 +0300

    feat: wip

--- src/options.ts
+++ src/options.ts
@@ -1,1 +1,37 @@
 import '../styles/options.scss';
+
+const button = document.getElementById('requestPermission');
+
+button.onclick = () => {
+  console.log('ya');
+  // @ts-ignore
+  navigator.getUserMedia =
+    // @ts-ignore
+    navigator.getUserMedia ||
+    // @ts-ignore
+    navigator.webkitGetUserMedia ||
+    // @ts-ignore
+    navigator.mozGetUserMedia;
+
+  // @ts-ignore
+  if (navigator.getUserMedia) {
+    // @ts-ignore
+    navigator.getUserMedia(
+      { audio: false, video: { width: 1280, height: 720 } },
+
+      // @ts-ignore
+      (stream) => {
+        {
+          console.log('success');
+        }
+      },
+
+      // @ts-ignore
+      (err) => {
+        console.error(`The following error occurred: ${err.name}`);
+      },
+    );
+  } else {
+    console.log('getUserMedia not supported');
+  }
+};
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -8,6 +8,32 @@ chrome.runtime.onInstalled.addListener(async () => {
   console.log('Extension successfully installed!');
 });
 
+chrome.runtime.onMessage.addListener(
+  (message: string, sender, sendResponse) => {
+    console.log(message);
+
+    chrome.windows.getLastFocused(
+      // Without this, window.tabs is not populated.
+      { populate: true },
+      function (window) {
+        var foundSelected = false;
+        for (var i = 0; i < window.tabs.length; i++) {
+          // Finding the selected tab.
+          if (window.tabs[i].active) {
+            foundSelected = true;
+          }
+          // Finding the next tab.
+          else if (foundSelected) {
+            // Selecting the next tab.
+            chrome.tabs.update(window.tabs[i].id, { active: true });
+            return;
+          }
+        }
+      },
+    );
+  },
+);
+
 // Log storage changes, might be safely removed
 chrome.storage.onChanged.addListener((changes) => {
   for (const [key, value] of Object.entries(changes)) {
